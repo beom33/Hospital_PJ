e36a5f7c0206fcc0a43d281fad13f24d8f6393eb
beom33 <byby5546@naver.com>
2026-01-24 00:04:19 +0900

    .

--- backend/src/main/java/com/medical/portfolio/MedicalPortfolioApplication.java
+++ backend/src/main/java/com/medical/portfolio/MedicalPortfolioApplication.java
@@ -7,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MedicalPortfolioApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MedicalPortfolioApplication.class, args);
-    }
+        SpringApplication.run(MedicalPortfolioApplication.class, args); }
 }
